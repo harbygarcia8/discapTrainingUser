d27c99800daeb8c9c1b70495570922697df99082
harby garcia <harby-96@hotmail.com>
2023-02-15 21:27:39 -0500

    [Create] Iniciando microservicio api user

---
+++ apidiscapuser/src/main/java/com/discaptraining/apidiscapuser/ApidiscapuserApplication.java
@@ -0,0 +1,13 @@
+package com.discaptraining.apidiscapuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidiscapuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidiscapuserApplication.class, args);
+	}
+
+}
---
+++ apidiscapuser/src/test/java/com/discaptraining/apidiscapuser/ApidiscapuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discaptraining.apidiscapuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidiscapuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
