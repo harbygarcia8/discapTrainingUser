a82d12d18e7ade82b4a867af4542ca0d229ef999
DESKTOP-DHM9C04\Felipe <pipeeuseordonez@gmail.com>
2023-03-16 12:56:46 -0500

    no message

--- apidiscapuser/src/main/java/com/discaptraining/apidiscapuser/config/JwtAuthenticationEntryPoint.java
+++ apidiscapuser/src/main/java/com/discaptraining/apidiscapuser/config/JwtAuthenticationEntryPoint.java
@@ -2,10 +2,11 @@ package com.discaptraining.apidiscapuser.config;
 import java.io.IOException;
 import java.io.Serializable;
 
+import javax.naming.AuthenticationException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +16,10 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 
     private static final long serialVersionUID = -7858869558953243875L;
 
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
 
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
